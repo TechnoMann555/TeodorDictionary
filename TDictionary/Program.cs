0d1b24f381c2181247c54968f3e0ae141fb93269
Teodor <sesumt@rocketmail.com>
2024-10-05 22:19:21 +0200

    Implemented the insertion and fetching operations. Wrote a method for testing that prints out the entire hash-table.

--- TDictionary/Program.cs
+++ TDictionary/Program.cs
@@ -12,6 +12,23 @@ namespace TDictionary
         {
             TeodorDictionary<string, int> dict = new TeodorDictionary<string, int>(20);
 
+            try
+            {
+                dict.Insert("Beograd", 1000000);
+                dict.Insert("Novi Sad", 2000000);
+                dict.Insert("aAFAEF", 3000000);
+
+                Console.WriteLine(dict.GetValue("Beograd"));
+                Console.WriteLine(dict.GetValue("Novi Sad"));
+                Console.WriteLine(dict.GetValue("aAFAEF"));
+
+                dict.PrintList();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             Console.ReadLine();
         }
     }
--- TDictionary/TeodorDictionary.cs
+++ TDictionary/TeodorDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 
 namespace TDictionary
@@ -25,17 +26,14 @@ namespace TDictionary
 			table = new LinkedList<KeyValuePair<TKey, TValue>>[initialSize];
 		}
 
-		private int ItemCount
-        { 
-			get { return itemCount; }
-			set { itemCount = value; }
-		}
+		public int Count
+        { get { return itemCount; } }
 
 		private int TotalBucketCount
         { get { return table.Length; } }
 
-		private int UsedBucketCount
-        { get { return usedBuckets; } }
+		private double LoadFactor
+        { get { return (double)usedBuckets / TotalBucketCount; } }
 
 		
 		// Gets the hash code of a given object
@@ -59,6 +57,96 @@ namespace TDictionary
 			return this.HashFunction(key) % TotalBucketCount;
         }
 
+		// Inserts a new key-value pair into the hash table
+		public void Insert(TKey key, TValue value)
+        {
+			int arrayIndex = HashKey(key);
+			KeyValuePair<TKey, TValue> newPair = new KeyValuePair<TKey, TValue>(key, value);
 
+			// If the bucket is empty, create a new linked list
+			if(table[arrayIndex] == null)
+				table[arrayIndex] = new LinkedList<KeyValuePair<TKey, TValue>>();
+
+			LinkedList<KeyValuePair<TKey, TValue>> bucketList = table[arrayIndex];
+
+			// Bucket list is empty - add the first node
+			if(bucketList.Count == 0)
+			{
+				bucketList.AddFirst(newPair);
+				usedBuckets++;
+			}
+
+			else
+			{
+				// Check if a key-value pair with the passed key value already exists in the bucket
+				foreach(KeyValuePair<TKey, TValue> pair in bucketList)
+				{
+					if(pair.Key.Equals(newPair.Key))
+						throw new ArgumentException("An item with the given key already exists!");
+				}
+				// Add the new pair to the end of the list
+				bucketList.AddLast(newPair);
+			}
+
+			itemCount++;
+        }
+
+		public TValue GetValue(TKey key)
+        {
+			int arrayIndex = HashKey(key);
+			LinkedList<KeyValuePair<TKey, TValue>> bucketList = table[arrayIndex];
+
+			// The bucket is empty
+			if(bucketList == null)
+				throw new ArgumentException("An item with the given key does not exist!");
+
+			// Only one item is in the bucket - therefore, no collision had occured 
+			if(bucketList.Count == 1)
+            {
+				return bucketList.First.Value.Value;
+            }
+			
+			// Multiple items are in the same bucket - therefore,
+			// collisions had occured, so search for the matching key-value pair
+			else
+            {
+				foreach(KeyValuePair<TKey, TValue> pair in bucketList)
+                {
+					if(pair.Key.Equals(key))
+						return pair.Value;
+				}
+
+				// No matching pair has been found
+				throw new ArgumentException("An item with the given key does not exist!");
+            }
+		}
+
+		// METHOD MADE FOR TESTING - prints the entire hash table structure
+		public void PrintList()
+        {
+			StringBuilder output = new StringBuilder();
+			int index = 0;
+
+			foreach(LinkedList<KeyValuePair<TKey, TValue>> bucket in table)
+            {
+				output.Append($"[{index}]-> ");
+
+				if(bucket != null)
+                {
+					foreach(KeyValuePair<TKey, TValue> pair in bucket)
+					{
+						output.Append($"<{pair.Key.ToString()}, {pair.Value.ToString()}>-");
+					}
+				}
+				else
+                {
+					output.Append("Empty");
+                }
+				output.AppendLine();
+				index++;
+            }
+
+			Console.WriteLine(output.ToString());
+        }
 	}
 }

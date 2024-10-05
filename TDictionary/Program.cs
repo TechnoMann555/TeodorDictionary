ec7e401402c3209b3be87f710bc9fec761349871
Teodor <sesumt@rocketmail.com>
2024-10-05 17:43:59 +0200

    Created the TeodorDictionary class with the hashtable array, two constructors and some constants.

---
+++ TDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeodorDictionary<string, int> dict = new TeodorDictionary<string, int>(20);
+        }
+    }
+}
---
+++ TDictionary/TeodorDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDictionary
+{
+	public class TeodorDictionary<TKey, TValue>
+	{
+		// The Seperate Chaining collision resolution policy requires us
+		// to organize the data as an array of linked lists of key-value pairs
+		private LinkedList<KeyValuePair<TKey, TValue>>[] table;
+
+		private const double maxLoadFactor = 0.7;
+		private const int defaultInitialSize = 10;
+
+		public TeodorDictionary()
+		{
+			table = new LinkedList<KeyValuePair<TKey, TValue>>[defaultInitialSize];
+		}
+
+		public TeodorDictionary(int initialSize)
+        {
+			table = new LinkedList<KeyValuePair<TKey, TValue>>[initialSize];
+		}
+	}
+}
